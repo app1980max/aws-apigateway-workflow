e76c15e276ba45ff5d9108aad32a5a7d991ebd7b
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-05 15:50:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<img width="1536" height="1236" alt="image" src="https://github.com/user-attachments/assets/af2a8072-679b-4e0b-b240-2634d0375475" />
+
+## Lambda ETL | AWS 
+Serverless ETL pipeline built with AWS Lambda, designed to ingest JSON data (e.g., from Facebook) and store it into a PostgreSQL database hosted in AWS RDS. The entire infrastructure is provisioned using Terraform, including VPC networking, security groups, IAM roles, and Lambda configuration.
+
+
+
+🎯 Architecture Overview
+```
+AWS Lambda: Processes JSON data and writes it to PostgreSQL.
+VPC & Subnets: Lambda runs in isolated subnets with proper network access.
+Security Group: Controls inbound/outbound access between Lambda and PostgreSQL.
+PostgreSQL RDS: Stores the transformed data in a secure, private subnet.
+IAM Role: Grants Lambda necessary permissions for CloudWatch logging and VPC ENI creation.
+Internet Gateway: Allows Lambda access to external APIs (e.g., Facebook API).
+```
+
+
+🧱 Features
+```
+✅ Fully automated provisioning with Terraform
+✅ High availability using multiple subnets in different Availability Zones
+✅ Secure connectivity between Lambda and RDS
+✅ Configurable environment variables for database credentials
+✅ Easy to extend for other JSON data source
+```
+
+
+
+🚀 Deployment Options
+```
+terraform init
+terraform validate
+terraform plan -var-file="template.tfvars"
+terraform apply -var-file="template.tfvars" -auto-approve
+```
+
---
+++ s3.tf
@@ -0,0 +1,69 @@
+# ----------------------
+# Create S3 Bucket
+# ----------------------
+resource "aws_s3_bucket" "etl_bucket" {
+  depends_on = [aws_nat_gateway.nat]
+  bucket     = "facebook-etl-bucket-${random_id.bucket_suffix.hex}"
+
+  # Modern best practice: disable ACLs and enforce ownership
+  #object_ownership = "BucketOwnerEnforced"
+
+  tags = {
+    Name        = "facebook-etl-bucket"
+    Environment = "dev"
+  }
+
+  force_destroy = true # allows deletion even if objects exist
+}
+
+# Random suffix for unique bucket name
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+
+# ----------------------
+# Lambda S3 IAM Policy
+# ----------------------
+resource "aws_iam_policy" "lambda_s3_policy" {
+  name        = "lambda-s3-access"
+  description = "Allow Lambda to read/write S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.etl_bucket.arn,
+          "${aws_s3_bucket.etl_bucket.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_s3_policy.arn
+}
+
+
+# ----------------------
+# Upload Data to S3 Bucket
+# ----------------------
+resource "aws_s3_bucket_object" "etl_json" {
+  bucket       = aws_s3_bucket.etl_bucket.id
+  key          = "etl-data/config.json"   # path in bucket
+  source       = "config/config.json"     # local file
+  content_type = "application/json"
+
+  # Ensure the bucket is created first
+  depends_on = [aws_s3_bucket.etl_bucket]
+}
