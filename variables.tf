1d4e0f560f8dc61f7aa63479278f350a1e7e033c
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-05 15:55:40 +0200

    update

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "lambda_function_name" {
+  default = "serverless-api-handler"
+}
+
+variable "api_name" {
+  default = "serverless-api"
+}
+
+variable "dynamodb_table_name" {
+  default = "serverless-data"
+}
