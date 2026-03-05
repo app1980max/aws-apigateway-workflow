1b4c27d18b7ab6e25335832bc20d8f78a9410421
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-05 17:31:20 +0200

    update

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 
 output "api_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/data"
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/data"
 }
