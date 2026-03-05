e6c7e9855d41c813f3044acc6f2245bd17b4dbf7
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-05 15:57:40 +0200

    update

---
+++ lambda.tf
@@ -0,0 +1,22 @@
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambda_function.py"
+  output_path = "${path.module}/lambda/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.data_store.name
+    }
+  }
+}
